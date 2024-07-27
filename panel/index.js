fcd8d132ab977ff672c7d72da864267830dade66
Sam <semkamirov@gmail.com>
2024-07-27 14:49:11 +0500

    Добавляет автоматическое определение адреса

--- panel/index.js
+++ panel/index.js
@@ -1,6 +1,28 @@
 const { readFileSync } = require("fs");
 const http = require("http");
 const url = require("url");
+const ip = require("ip");
+const fs = require("fs");
+
+const INDEX = "./index.html";
+const SEVEN = "./7/7.html";
+const MONTH = "./30/30.html";
+const PORT = 3000;
+
+let serverAddress = ip.address();
+
+const replaceAddress = (filename) => {
+  fs.readFile(filename, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var result = data.replace(/server/g, `http://${serverAddress}:${PORT}`);
+
+    fs.writeFile(filename, result, "utf8", function (err) {
+      if (err) return console.log(err);
+    });
+  });
+};
 
 let server = new http.Server((req, res) => {
   res.writeHead(200, {
@@ -27,4 +49,9 @@ let server = new http.Server((req, res) => {
   }
 });
 
-server.listen(3000, "localhost");
+replaceAddress(INDEX);
+replaceAddress(SEVEN);
+replaceAddress(MONTH);
+
+server.listen(PORT, serverAddress);
+console.log("Server started on " + serverAddress + ":3000");
